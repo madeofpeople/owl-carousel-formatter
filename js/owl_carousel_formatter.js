97d76c4d009ec53519333bd4fbe402546e27d004
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-11-04 14:02:29 -0800

    fixed issues with settings, javascript tweaks to make sure we get clean boolean values (owl carousel requires this)

--- js/owl_carousel_formatter.js
+++ js/owl_carousel_formatter.js
@@ -1,14 +1,18 @@
-(function($, Drupal){
-  Drupal.behaviors.galleryformatter = {
-  attach: function (context, settings ) {
-    console.log("Drupal.behaviors.galleryformatter:", settings );
-    $( '.owl-carousel-formatter:not(.instantiated)', context ).each( function(){
-      $(this).owlCarousel({
-        navigation : false,
-        singleItem : true,
-        lazyLoad : true
-      });
-    }).addClass( 'instantiated' );
+(function($){
+  Drupal.behaviors.owl_carousel_formatter = {
+  attach: function( context, settings ) {
+    // owl carrousel uses a lot of "===" and seems to break if pagination is set to 1 or 'true' instead of a real boolean.
+    // so we have to sanitize it, this kind of pains me, of course right now any value that would need to be a numeric one or zero would sort of break.
+    for( var i in settings.owl_carousel_formatter ){
+      if( settings.owl_carousel_formatter[i] === 1 || settings.owl_carousel_formatter[i] === 0 ){
+        settings.owl_carousel_formatter[i] = Boolean( settings.owl_carousel_formatter[i] );
+      }
+    };
+    var carousel = $( '.owl-carousel-formatter:not(.instantiated)', context );
+    carousel.each( function(){
+      $(this).owlCarousel( settings.owl_carousel_formatter );
+      $(this).addClass( 'instantiated' );
+    })
   }
 }
-}(jQuery,Drupal));
+}(jQuery));
