25c074d234bbe33a40f9ea9b83ca426bce71937f
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-11-05 00:00:20 -0800

    cleans up handing over of variables between drupal and  the OWL carousel.

--- js/owl_carousel_formatter.js
+++ js/owl_carousel_formatter.js
@@ -3,9 +3,13 @@
   attach: function( context, settings ) {
     // owl carrousel uses a lot of "===" and seems to break if pagination is set to 1 or 'true' instead of a real boolean.
     // so we have to sanitize it, this kind of pains me, of course right now any value that would need to be a numeric one or zero would sort of break.
-    for( var i in settings.owl_carousel_formatter ){
-      if( settings.owl_carousel_formatter[i] === 1 || settings.owl_carousel_formatter[i] === 0 ){
-        settings.owl_carousel_formatter[i] = Boolean( settings.owl_carousel_formatter[i] );
+    for( var key in settings.owl_carousel_formatter ){
+      var val = settings.owl_carousel_formatter[key];
+      if( settings.owl_carousel_formatter === null ){
+        settings.owl_carousel_formatter[key] = null;
+        delete settings.owl_carousel_formatter[key];
+      }else if(  === 1 || settings.owl_carousel_formatter[key] === 0 ){
+        settings.owl_carousel_formatter[key] = Boolean( val );
       }
     };
     var carousel = $( '.owl-carousel-formatter:not(.instantiated)', context );
